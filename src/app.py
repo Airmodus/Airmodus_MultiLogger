ec4ccce808de55f285bcdebf84aad92dc9da9704
Miikka Lehto <miikka.lehto@hotmail.com>
2024-12-09 16:42:33 +0200

    version 0.9.2

--- src/app.py
+++ src/app.py
@@ -22,7 +22,7 @@ from pyqtgraph import GraphicsLayoutWidget, DateAxisItem, AxisItem, ViewBox, Plo
 from pyqtgraph.parametertree import Parameter, ParameterTree, parameterTypes
 
 # current version number displayed in the GUI (Major.Minor.Patch or Breaking.Feature.Fix)
-version_number = "0.9.1"
+version_number = "0.9.2"
 
 # Define instrument types
 CPC = 1
